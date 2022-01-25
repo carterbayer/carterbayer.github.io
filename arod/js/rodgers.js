2dfff9b361dcf3c7ee0d3fcbf40d76541e28888b
carterbayer <cabayer@eastsideprep.org>
2022-01-25 14:44:43 -0800

    Condition Meters Centered

--- arod/js/rodgers.js
+++ arod/js/rodgers.js
@@ -1,1 +1,15 @@
-console.log("hello Pikachu!")
+let skill = 0;
+let teammates = 0;
+let money = 0;
+
+function teammatesRodgers() {
+    console.log("Bring Rodgers teammates...")
+}
+
+function trainRodgers() {
+    console.log("Train Rodgers...")
+}
+
+function payRodgers() {
+    console.log("Paying Rodgers...")
+}
